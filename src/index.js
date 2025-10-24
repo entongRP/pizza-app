8c369549151d65737b38a37e4f97b6af8f89e5fa
entongRP <24012306@myrp.edu.sg>
2025-10-24 21:21:35 +0800

    final 1.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./index.css";
 
 function App() { 
     return (
@@ -12,18 +13,55 @@ function App() {
 }
 
 function Header() { 
-    return <h1>Andy's Pizza Co.</h1>;
+    return <h1 className='header'>Andy's Pizza Co.</h1>;
 }
 
-function Pizza () {
-    return (
-      <div>
-      <img src="/pizzas/funghi.jpg" alt="funghi pizza"/>
-      <h2>Funghi Pizza</h2>
-      <d2>Mushroom & cream</d2>
-      <h3>10</h3>
+function Pizza() {
+  return (
+    <div className="pizza-menu">
+      <div className="pizza-card">
+        <img src="/pizzas/funghi.jpg" alt="funghi pizza" className="pizza-img" />
+        <h2>Funghi Pizza</h2>
+        <p>Cheese, mushroom, onions, tomatoes</p>
+        <h3>$11</h3>
       </div>
-    );
+
+      <div className="pizza-card">
+        <img src="/pizzas/focaccia.jpg" alt="focaccia pizza" className="pizza-img" />
+        <h2>Focaccia Pizza</h2>
+        <p>Cheese, meat, herbs, tomato sauce</p>
+        <h3>$15</h3>
+      </div>
+
+      <div className="pizza-card">
+        <img src="/pizzas/margherita.jpg" alt="Margherita pizza" className="pizza-img" />
+        <h2>Margherita Pizza</h2>
+        <p>Cheese, herbs, tomato sauce</p>
+        <h3>$10</h3>
+      </div>
+
+      <div className="pizza-card">
+        <img src="/pizzas/prosciutto.jpg" alt="prosciutto pizza" className="pizza-img" />
+        <h2>Prosciutto Pizza</h2>
+        <p>Cheese, herbs, tomatoes</p>
+        <h3>$11</h3>
+      </div>
+
+      <div className="pizza-card">
+        <img src="/pizzas/salamino.jpg" alt="salamino pizza" className="pizza-img" />
+        <h2>Salamino Pizza</h2>
+        <p>Cheese, meat</p>
+        <h3>$12</h3>
+      </div>
+
+      <div className="pizza-card">
+        <img src="/pizzas/spinaci.jpg" alt="spinaci pizza" className="pizza-img" />
+        <h2>Spinaci Pizza</h2>
+        <p>Cheese, spinach</p>
+        <h3>$12</h3>
+      </div>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
