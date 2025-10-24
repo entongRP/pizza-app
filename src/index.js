d5f8c1deedba5a97c36a45db680a5f52a68cd73f
entongRP <24012306@myrp.edu.sg>
2025-10-24 10:59:56 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() { 
+    return (
+    <div>
+        {/* <h1>Andy's Pizza Co.</h1> */}
+        <Header />
+        <Pizza />
+    </div>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() { 
+    return <h1>Andy's Pizza Co.</h1>;
+}
+
+function Pizza () {
+    return (
+      <div>
+      <img src="/pizzas/funghi.jpg" alt="funghi pizza"/>
+      <h2>Funghi Pizza</h2>
+      <d2>Mushroom & cream</d2>
+      <h3>10</h3>
+      </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
