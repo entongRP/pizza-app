716043c4b5ddf724ba6749b2ed615c4a45de9959
entongRP <24012306@myrp.edu.sg>
2025-11-10 09:01:02 +0800

    redo

--- src/index.js
+++ src/index.js
@@ -2,67 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
 
-function App() { 
+function Header() {
+    return <h1
+    style = {{color:"orange", fontSize: "48px", textTransform: 'uppercase'}}
+    >Andy's Pizza App</h1>;
+}
+
+function App () {
     return (
-    <div>
-        {/* <h1>Andy's Pizza Co.</h1> */}
-        <Header />
-        <Pizza />
-    </div>
+        <div className='container'>
+            <Header/>
+            <Menu />
+            <Footer />
+        </div>
     );
 }
 
-function Header() { 
-    return <h1 className='header'>Andy's Pizza Co.</h1>;
+function Menu() {
+    return (
+        <div className='menu'>
+            <h2>Our Menu</h2>
+            <div className='pizzas'>
+                <Pizza/>
+                <Pizza/>
+            </div>
+        </div>
+    );
 }
 
-function Pizza() {
-  return (
-    <div className="pizza-menu">
-      <div className="pizza-card">
-        <img src="/pizzas/funghi.jpg" alt="funghi pizza" className="pizza-img" />
+function  Pizza() {
+    return (
+      <div className="pizza">
+        <img src="/pizzas/funghi.jpg" alt="funghi pizza" />
         <h2>Funghi Pizza</h2>
-        <p>Cheese, mushroom, onions, tomatoes</p>
-        <h3>$11</h3>
+        <p className='pizza p'>Cheese, mushroom, onions, tomatoes</p>
+        <h3 className='pizza h3'>$11</h3>
       </div>
+      
+    );
+}
 
-      <div className="pizza-card">
-        <img src="/pizzas/focaccia.jpg" alt="focaccia pizza" className="pizza-img" />
-        <h2>Focaccia Pizza</h2>
-        <p>Cheese, meat, herbs, tomato sauce</p>
-        <h3>$15</h3>
-      </div>
-
-      <div className="pizza-card">
-        <img src="/pizzas/margherita.jpg" alt="Margherita pizza" className="pizza-img" />
-        <h2>Margherita Pizza</h2>
-        <p>Cheese, herbs, tomato sauce</p>
-        <h3>$10</h3>
-      </div>
-
-      <div className="pizza-card">
-        <img src="/pizzas/prosciutto.jpg" alt="prosciutto pizza" className="pizza-img" />
-        <h2>Prosciutto Pizza</h2>
-        <p>Cheese, herbs, tomatoes</p>
-        <h3>$11</h3>
-      </div>
-
-      <div className="pizza-card">
-        <img src="/pizzas/salamino.jpg" alt="salamino pizza" className="pizza-img" />
-        <h2>Salamino Pizza</h2>
-        <p>Cheese, meat</p>
-        <h3>$12</h3>
-      </div>
-
-      <div className="pizza-card">
-        <img src="/pizzas/spinaci.jpg" alt="spinaci pizza" className="pizza-img" />
-        <h2>Spinaci Pizza</h2>
-        <p>Cheese, spinach</p>
-        <h3>$12</h3>
-      </div>
-    </div>
+function Footer() {
+    const currentHour = new Date().getHours();
+    const isOpen = currentHour >= 10 && currentHour <= 22;
+  return (
+    <footer className='footer'>
+        {isOpen ? "We're currently open" : "Sorry we're closed"}
+    </footer>
   );
 }
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
